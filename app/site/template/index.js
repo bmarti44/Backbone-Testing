327eceb5138edbf45a2a0841d2b43eca575395c8
Brian Martin <bmarti44@students.kennesaw.edu>
2013-11-04 22:20:07 -0500

    Working on integrating grunt and dust templating

---
+++ app/site/template/index.js
@@ -0,0 +1,5 @@
+define(["dust"],function (dust) {
+	// index.dust
+	(function(){dust.register("index",body_0);function body_0(chk,ctx){return chk.write("this is some dust!");}return body_0;})();
+	return "index";
+});
