baa017ffa32dd0f72c49e6651a37e83b14a90f27
Brian Martin <brian@brianmartin.com>
2013-11-05 16:55:25 -0500

    Tying dust templating into one of these callbacks

---
+++ app/site/template/about/index.js
@@ -0,0 +1,5 @@
+define(["dust"],function (dust) {
+	// about/index.dust
+	(function(){dust.register("about/index",body_0);function body_0(chk,ctx){return chk;}return body_0;})();
+	return "about/index";
+});
